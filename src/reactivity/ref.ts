fbbc29077ea3c53644c324f2f036ccdadeeba752
linhuizi <529246402@qq.com>
2022-06-29 17:00:01 +0800

    实现isRef和unRef功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,10 +1,12 @@
 import { hasChanged, isObject } from '../shared';
+import { ReactiveFlags } from './baseHandle';
 import {trackEffect, triggerEffect,isTracking} from './effect'
 import { reactive } from './reactive';
 class Ref{
   private _value:any;
   private _rawValue:any;
   public deps;
+  public v_isRef = true
   constructor(value){
     // 判断value是否是一个对象 是对象则按reactive处理 否则不处理
     this._rawValue = value
@@ -30,4 +32,13 @@ function convert(value){
 // ref 触发依赖收集
 export function ref(raw){
   return new Ref(raw)
+}
+// 检查值是否为一个 ref 对象
+export function isRef(ref){
+  return !!ref.v_isRef
+}
+// 如果参数是一个 ref，则返回内部值，否则返回参数本身。
+// 这是 val = isRef(val) ? val.value : val 的语法糖函数。
+export function unRef(ref){
+  return isRef(ref) ? ref.value : ref
 }
--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
-import { ref } from '../ref'
+import { isRef, ref, unRef } from '../ref'
 import { effect } from '../effect'
+import { reactive } from '../reactive';
 
 describe("ref", () => {
   // 实现基础的ref
@@ -41,5 +42,23 @@ describe("ref", () => {
     expect(dummy).toBe(1);
     a.value.count = 2;
     expect(dummy).toBe(2);
+  });
+  it("isRef", () => {
+    const a = ref(1);
+    const user = reactive({
+      age: 1,
+    });
+    const b = 10
+    expect(isRef(a)).toBe(true);
+    expect(isRef(1)).toBe(false);
+    expect(isRef(user)).toBe(false);
+    expect(isRef(b)).toBe(false);
+  });
+  it("unRef", () => {
+    const a = ref(1);
+    const b = 10
+    expect(unRef(a)).toBe(1);
+    expect(unRef(1)).toBe(1);
+    expect(unRef(b)).toBe(10);
   });
 });
