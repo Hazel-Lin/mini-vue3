07454cebcef53e7c1870aea574c84f733a20850d
linhuizi <529246402@qq.com>
2022-07-02 21:49:44 +0800

    update shared

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,9 +1,11 @@
 export const isObject = (val: unknown): val is Record<any, any> =>
   val !== null && typeof val === 'object'
 
- 
+export const isString = (value) => typeof value === "string";
 export const extend = Object.assign
 // 判断是否需要改变
 export const hasChanged = (newValue,oldValue)=> { 
   return !Object.is(newValue,oldValue)
 }
+export const hasOwn = (val, key) =>
+  Object.prototype.hasOwnProperty.call(val, key);
