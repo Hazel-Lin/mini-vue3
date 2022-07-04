96a4d9c33f87bcfc4beae9de28ad7635b41902b5
linhuizi <529246402@qq.com>
2022-07-04 21:39:04 +0800

    feat: update component

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -38,8 +38,5 @@ function handleSetupResult(instance, setupResult: any) {
 
 function finishComponentSetup(instance: any) {
   const Component = instance.type;
-
-  if (!Component.render) {
-    instance.render = Component.render;
-  }
+  instance.render = Component.render;
 }
